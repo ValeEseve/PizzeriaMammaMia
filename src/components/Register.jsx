d69de62ad3a41899e6363ff6ed50320e0bd9f36d
Valentina Segura Vargas <vale.eseve@gmail.com>
2025-06-06 01:45:27 -0400

    Agregado Register.jsx

---
+++ src/components/Register.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+const Register = () => {
+  return (
+    <section>
+      <form action="submit">
+        <h1>Login</h1>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </section>
+  )
+}
+
+export default Register
